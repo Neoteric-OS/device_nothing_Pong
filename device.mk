869dbf5663baf1bc3316416b75c54c546b88d1c9
HELLBOY017 <abhaygill017@gmail.com>
2023-10-26 12:26:36 +0530

    Pong: Build fingerprint HAL

--- device.mk
+++ device.mk
@@ -147,6 +147,9 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.nothing
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
