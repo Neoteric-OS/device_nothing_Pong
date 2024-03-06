4e729b9e4d1c25004e9df725e648f45c7cc8e6a6
Abhay Singh Gill <abhaygill017@gmail.com>
2024-03-06 19:05:06 +0530

    Pong: Reorganize init scripts
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- device.mk
+++ device.mk
@@ -234,7 +234,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/init/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Keymaster
 PRODUCT_PACKAGES += \
