5d8ee4e610d828e912d7475037fcd0c6b75dfc18
Abhay Singh Gill <abhaygill017@gmail.com>
2024-03-26 00:38:37 +0530

    Pong: BoardConfig: Fix derp
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,8 +85,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.nothing
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/device_framework_matrix.xml \
-    $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml \
-
+    $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
