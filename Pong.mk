f02cdfedbcaa67151dae24782b62296cabacbebf
Abhay Singh Gill <abhaygill017@gmail.com>
2024-06-08 15:44:26 +0530

    Pong: Setup Neoteric
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

---
+++ Pong.mk
@@ -0,0 +1,31 @@
+#
+# # Copyright (C) 2023 Neoteric OS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from Pong device
+$(call inherit-product, device/nothing/Pong/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_NAME := Pong
+PRODUCT_DEVICE := Pong
+PRODUCT_MANUFACTURER := nothing
+PRODUCT_BRAND := Nothing
+PRODUCT_MODEL := A065
+
+# Bootanimation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Override device name for Play Store.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=Pong \
+    TARGET_PRODUCT=Pong
+
+PRODUCT_GMS_CLIENTID_BASE := android-nothing
