35822bb0650fb05ca39fd182cce05c0feaf812d5
Abhay Singh Gill <abhaygill017@gmail.com>
2024-08-16 13:06:42 +0530

    Pong: Add mixer paths config from stock
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- device.mk
+++ device.mk
@@ -49,7 +49,8 @@ $(call inherit-product, hardware/dolby/dolby.mk)
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/audio_effects.xml \
-    $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape_qssi/audio_policy_configuration.xml
+    $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape_qssi/audio_policy_configuration.xml \
+    $(DEVICE_PATH)/configs/audio/mixer_paths_waipio_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/mixer_paths_waipio_qrd.xml
 
 # Authsecret
 PRODUCT_PACKAGES += \
