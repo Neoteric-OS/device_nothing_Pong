d37617ed76399877e2f901897ac5827a630bef07
Bruno Martins <bgcngm@gmail.com>
2024-09-24 17:23:18 +0900

    Pong: Convert WiFi firmware symlinks to install_symlink targets
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -331,6 +331,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.nt
 
 # WLAN
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6490/WCNSS_qcom_cfg.ini
 
