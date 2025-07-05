2d81f968af0be828ae7469608eacaeaceb29e5b4
SagarMakhar <sagarmakhar@gmail.com>
2025-07-05 18:24:30 +0530

    Pong: Turn off persist.vendor.radio.poweron_opt
    Set to 1 in qcril_database migrations. Sed the property
    to something else so it defaults to 0.
    
    If this value 1 it breaks SMS receiving.
    
    Change-Id: I7f56ecaac51abb6525c6022d92eac1c285ab0688
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -80,6 +80,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
             ;;
+        vendor/lib64/libqcrilNr.so|vendor/lib64/libril-db.so)
+            [ "$2" = "" ] && return 0
+            sed -Ei 's/persist\.vendor\.radio\.poweron_opt/persist.vendor.radio.poweron_ign/g' "${2}"
+            ;;
         *)
             return 1
             ;;
