f4a6900815ef92718ae6f0c8bc585e2141926641
chandu078 <chandudyavanapelli03@gmail.com>
2025-08-05 10:30:16 +0530

    Pong: Add NT charge service
    Co-authored-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Change-Id: I8937714c24670a4b886890d4f53e6ab9b83b802c

--- extract-files.sh
+++ extract-files.sh
@@ -84,6 +84,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -Ei 's/persist\.vendor\.radio\.poweron_opt/persist.vendor.radio.poweron_ign/g' "${2}"
             ;;
+        vendor/bin/hw/vendor.noth.hardware.charge-service)
+            [ "$2" = "" ] && return 0
+            grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
