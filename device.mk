9b51bb6d0f0e805b6be27d885e8f02c8bd8c5b06
Abhay Singh Gill <abhaygill017@gmail.com>
2025-08-09 18:15:30 +0530

    [SQUASH] Pong: Remove nothing charging control
    Revert "Pong: charging: Ship in recovery as well"
    
    This reverts commit 9a6ef4c10a319c3a9c89c0c510ad317281a706ad.
    
    Revert "Pong: init: Run charging control service when using offline charging"
    
    This reverts commit 621a039a40b8a50041a3fc8475ef27540eeee833.
    
    Revert "Pong: Add nothing charging control service"
    
    This reverts commit b63dde589c2386bb924fc6ab0257cf7f37c223c8.

--- device.mk
+++ device.mk
@@ -82,11 +82,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.aon@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-# Charging
-PRODUCT_PACKAGES += \
-    nt-charging-control \
-    nt-charging-control_recovery
-
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
